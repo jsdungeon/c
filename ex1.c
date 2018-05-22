6c1a33b9ad497766f44a2c0e33697f97b138ed47
jsdungeon <git@jsdungeon.com>
2018-05-22 11:59:48 +0700

    Added a random number generator and set the zombie health to 100. The hit will cause a damage between 0 and 50. The health after receiving the hit will be displayed.

--- ex1.c
+++ ex1.c
@@ -1,15 +1,21 @@
 #include <stdio.h>
+#include <time.h> //We need this for the seed of the random generator
 
 /* This is a comment. */
 int main(int argc, char *argv[])
 {
+	srand( (unsigned)time( NULL) );
+	int hit = (rand() % 50); //We generate a random number between 0 and 50
 	int distance = 100;
 	int zombie = 4;
+	int zombie_health = 100;
 	int deadlyBlast = 1;
 
 	// this is also a comment
 	printf("You are %d miles away.\n", distance);
 	distance = distance * 5;
+	printf("You hit the Zombie and caused %d damage!\n",hit);
+	printf("The Zombie has %d health left.\n",(100 - hit));
 	printf("You just have been teleported to another place. You are now %d miles away.\n", distance);
 	printf("\nYou see %d Zombies arriving!\n", zombie);
 	zombie = zombie - deadlyBlast;
