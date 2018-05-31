21fb3b7356ffc09b63c74a5e4f956b17254c463c
jsdungeon <git@jsdungeon.com>
2018-05-31 18:48:39 +0700

    Changed array declaration to something that actually works. Added 25 rooms and their exits. Added check for possible exits and printing them out. This is static for room 1 so far. Deleted obsolete lines and comments.

--- ex1.c
+++ ex1.c
@@ -12,55 +12,67 @@ int main(int argc, char *argv[])
 	int deadlyBlast = 1;
 	int decision = 10;
 	int attack = 0;
-	int dungeon0[5][5]; //declaration of a two-dimensional array
-	int dungeon1[5][5];
-	int dungeon2[5][5];
-	int dungeon3[5][5];
-	/* The avove arrays are needed to create the map of the dungeon.
-	A 0 means there is no connection to the next room and a 1 means that There
-	is a connection to the next room.
-	Example:
-	dungeon0[0][0] = 0 means that we can't go north.
-	dungeon1[0][0] = 1 means that we can go east.
-	dungeon2[0][0] = 1 means that we can go south.
-	dungeon3[0][0 = 0 means that we can't go west.
-	*/
+	int room = 0;
+	int i;
 
-	/*below I declare a multi-dimensional array for testing the dungeon
-	architecture. This is the next iteration of the above two-dimensional arrays*/
-	int dungeon [5][5][0][0][0][0][0][0][0][0][0];
-	/*
-The 11 fields are representing this:
-0 + 1 = Coordinates of the room
-2,3,4,5 = Exits N,E,S,W
-7,9 = Up, down
-10 = Monster
-11 = Food
-12 = Weapon
-	*/
-	/*Here I am poulating the first room at [0][0]*/
-	dungeon [0][0][0][1][1][0][0][0][0][1][1];
+	//int dungeonRoom[8];
+	int dungeonRoom1[9] = {1,0,1,1,0,0,0,1,1};
+	int dungeonRoom2[9] = {2,0,1,0,1,0,0,1,1};
+	int dungeonRoom3[9] = {3,0,1,0,1,0,0,1,1};
+	int dungeonRoom4[9] = {4,0,1,0,1,0,0,1,1};
+	int dungeonRoom5[9] = {5,0,0,1,1,0,0,1,1};
+	int dungeonRoom6[9] = {6,1,1,0,0,0,0,1,1};
+	int dungeonRoom7[9] = {7,0,0,1,1,0,0,1,1};
+	int dungeonRoom8[9] = {8,0,1,1,0,0,0,1,1};
+	int dungeonRoom9[9] = {9,0,1,0,1,0,0,1,1};
+	int dungeonRoom10[9] = {10,1,0,0,1,0,0,1,1};
+	int dungeonRoom11[9] = {11,0,0,1,0,0,0,1,1};
+	int dungeonRoom12[9] = {12,1,0,1,0,0,0,1,1};
+	int dungeonRoom13[9] = {13,1,1,1,0,0,0,1,1};
+	int dungeonRoom14[9] = {14,0,1,0,1,0,0,1,1};
+	int dungeonRoom15[9] = {15,0,0,1,1,0,0,1,1};
+	int dungeonRoom16[9] = {16,1,1,1,0,0,0,1,1};
+	int dungeonRoom17[9] = {17,1,1,0,1,0,0,1,1};
+	int dungeonRoom18[9] = {18,1,0,0,1,0,0,1,1};
+	int dungeonRoom19[9] = {19,0,1,1,0,0,0,1,1};
+	int dungeonRoom20[9] = {20,1,0,1,1,0,0,1,1};
+	int dungeonRoom21[9] = {21,1,1,0,0,0,0,1,1};
+	int dungeonRoom22[9] = {22,0,1,0,1,0,0,1,1};
+	int dungeonRoom23[9] = {23,0,1,0,1,0,0,1,1};
+	int dungeonRoom24[9] = {24,1,0,0,1,0,0,1,1};
+	int dungeonRoom25[9] = {25,1,0,0,0,0,0,1,1};
 
-	dungeon0[0][0] = 0;
-	dungeon1[0][0] = 1;
-	dungeon2[0][0] = 1;
-	dungeon3[0][0] = 0;
-	/*The above declaration means that the first room 0,0 has exits to the
-	east and south*/
+room = dungeonRoom1[1];
+printf("You are in room: %d \n\n");
+
+	if (dungeonRoom1[1] ==1)
+			{
+				printf("There is an exit to the north\n\n");
+			}
+	if (dungeonRoom1[2] ==1)
+			{
+				printf("There is an exit to the east\n\n");
+			}
+	if (dungeonRoom1[3] ==1)
+				{
+					printf("There is an exit to the south\n\n");
+				}
+	if (dungeonRoom1[4] ==1)
+					{
+						printf("There is an exit to the west\n\n");
+					}
 
 
+printf("You are %d miles away.\n", distance);
+distance = distance * 5;
+printf("You hit the Zombie and caused %d damage!\n",hit);
+printf("The Zombie has %d health left.\n",(zombieHealth - hit));
+printf("You just have been teleported to another place. You are now %d miles away.\n", distance);
+printf("\nYou see %d Zombies arriving!\n", zombie);
+zombie = zombie - deadlyBlast;
+printf("You hit a Zombie hard and placed a deadly blast against his rotten skull!\nThere are %d Zombies left!\n", zombie);
+printf("\nEnter '0' for running away or '1' to fight:\n");
 
-	// this is also a comment
-	printf("You are %d miles away.\n", distance);
-	distance = distance * 5;
-	printf("You hit the Zombie and caused %d damage!\n",hit);
-	printf("The Zombie has %d health left.\n",(zombieHealth - hit));
-	printf("You just have been teleported to another place. You are now %d miles away.\n", distance);
-	printf("\nYou see %d Zombies arriving!\n", zombie);
-	zombie = zombie - deadlyBlast;
-	printf("You hit a Zombie hard and placed a deadly blast against his rotten skull!\nThere are %d Zombies left!\n", zombie);
-	printf("\nEnter '0' for running away or '1' to fight:\n");
-//	while( scanf( "%lf", &decision ) == 0 )
 	while( scanf( "%d", &decision ) == 0 )
 
 			printf("Your decision is: \n %d ", decision);
