6e5a973195b2c611fcf81361394bbf7616535f96
jsdungeon <git@jsdungeon.com>
2018-06-01 15:42:12 +0700

    Removed old arrays added new arrays I have now a bunch of arrys which make it possible to define a room number and check the linked room information like possible exits, monsters, weapons and food.

--- ex1.c
+++ ex1.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <time.h> //We need this for the seed of the random generator
 
-/* This is a comment. */
 int main(int argc, char *argv[])
 {
 	srand( (unsigned)time( NULL ) );
@@ -13,54 +13,63 @@ int main(int argc, char *argv[])
 	int decision = 10;
 	int attack = 0;
 	int room = 0;
-	int i;
+	int i = 0;
+	int extDungeonRoom[25][9];
+	int val = 0;
+	int testEx = 0;
 
-	//int dungeonRoom[8];
-	int dungeonRoom1[9] = {1,0,1,1,0,0,0,1,1};
-	int dungeonRoom2[9] = {2,0,1,0,1,0,0,1,1};
-	int dungeonRoom3[9] = {3,0,1,0,1,0,0,1,1};
-	int dungeonRoom4[9] = {4,0,1,0,1,0,0,1,1};
-	int dungeonRoom5[9] = {5,0,0,1,1,0,0,1,1};
-	int dungeonRoom6[9] = {6,1,1,0,0,0,0,1,1};
-	int dungeonRoom7[9] = {7,0,0,1,1,0,0,1,1};
-	int dungeonRoom8[9] = {8,0,1,1,0,0,0,1,1};
-	int dungeonRoom9[9] = {9,0,1,0,1,0,0,1,1};
-	int dungeonRoom10[9] = {10,1,0,0,1,0,0,1,1};
-	int dungeonRoom11[9] = {11,0,0,1,0,0,0,1,1};
-	int dungeonRoom12[9] = {12,1,0,1,0,0,0,1,1};
-	int dungeonRoom13[9] = {13,1,1,1,0,0,0,1,1};
-	int dungeonRoom14[9] = {14,0,1,0,1,0,0,1,1};
-	int dungeonRoom15[9] = {15,0,0,1,1,0,0,1,1};
-	int dungeonRoom16[9] = {16,1,1,1,0,0,0,1,1};
-	int dungeonRoom17[9] = {17,1,1,0,1,0,0,1,1};
-	int dungeonRoom18[9] = {18,1,0,0,1,0,0,1,1};
-	int dungeonRoom19[9] = {19,0,1,1,0,0,0,1,1};
-	int dungeonRoom20[9] = {20,1,0,1,1,0,0,1,1};
-	int dungeonRoom21[9] = {21,1,1,0,0,0,0,1,1};
-	int dungeonRoom22[9] = {22,0,1,0,1,0,0,1,1};
-	int dungeonRoom23[9] = {23,0,1,0,1,0,0,1,1};
-	int dungeonRoom24[9] = {24,1,0,0,1,0,0,1,1};
-	int dungeonRoom25[9] = {25,1,0,0,0,0,0,1,1};
+	int testExtDungeonRoom[25][9] = {
+		{1,0,1,1,0,0,0,1,1},
+		{2,0,1,0,1,0,0,1,1},
+		{3,0,1,0,1,0,0,1,1},
+		{4,0,1,0,1,0,0,1,1},
+		{5,0,0,1,1,0,0,1,1},
+		{6,1,1,0,0,0,0,1,1},
+		{6,1,1,0,0,0,0,1,1},
+		{8,0,1,1,0,0,0,1,1},
+		{9,0,1,0,1,0,0,1,1},
+		{10,1,0,0,1,0,0,1,1},
+		{11,0,0,1,0,0,0,1,1},
+		{12,1,0,1,0,0,0,1,1},
+		{13,1,1,1,0,0,0,1,1},
+		{14,0,1,0,1,0,0,1,1},
+		{15,0,0,1,1,0,0,1,1},
+		{16,1,1,1,0,0,0,1,1},
+		{17,1,1,0,1,0,0,1,1},
+		{18,1,0,0,1,0,0,1,1},
+		{19,0,1,1,0,0,0,1,1},
+		{20,1,0,1,1,0,0,1,1},
+//		{20,10,11,12,13,14,15,16,17},
+		{21,1,1,0,0,0,0,1,1},
+		{22,0,1,0,1,0,0,1,1},
+		{23,0,1,0,1,0,0,1,1},
+		{24,1,0,0,1,0,0,1,1},
+		{25,1,0,0,0,0,0,1,1}
+	};
 
-room = dungeonRoom1[1];
-printf("You are in room: %d \n\n");
+i = 18;
+//room = testExtDungeonRoom[i][0];
+room = 19;
+testEx = testExtDungeonRoom[i][1];
+printf("testExtDungeonRoom [0][0] value = %d\n\n",testEx );
+printf("You are in room %d\n\n",room );
 
-	if (dungeonRoom1[1] ==1)
+	if (testExtDungeonRoom[i][1] ==1)
 			{
 				printf("There is an exit to the north\n\n");
 			}
-	if (dungeonRoom1[2] ==1)
+	if (testExtDungeonRoom[i][2] ==1)
 			{
 				printf("There is an exit to the east\n\n");
 			}
-	if (dungeonRoom1[3] ==1)
-				{
-					printf("There is an exit to the south\n\n");
-				}
-	if (dungeonRoom1[4] ==1)
-					{
-						printf("There is an exit to the west\n\n");
-					}
+	if (testExtDungeonRoom[i][3] ==1)
+			{
+				printf("There is an exit to the south\n\n");
+			}
+	if (testExtDungeonRoom[i][4] ==1)
+			{
+				printf("There is an exit to the west\n\n");
+			}
 
 
 printf("You are %d miles away.\n", distance);
