f7636192e1cf67de1976beee7f7f00d555af2831
ianwuk <37680034+ianwuk@users.noreply.github.com>
2018-08-17 19:27:29 +0700

    First C exercises by Ian

---
+++ Ian/Chapter 1/Exercise 1/Chap1Ex1.c
@@ -0,0 +1,10 @@
+/* Prints a message on the screen */
+#include <stdio.h>
+main()
+{
+
+printf("Just one small step for coders.  One giant leap for");
+printf(" programmers!\n");
+return 0;
+
+}
---
+++ Ian/Chapter 1/Exercise 2/Chap1Ex2.c
@@ -0,0 +1,10 @@
+/* A program that uses the characters, integers and floating point data types */
+#include<stdio.h>
+main()
+{
+printf("I am learning the %c programming language \n", 'C');
+printf("I have just completed Chapter %d\n", 2);
+printf("I am %.1f percent ready to move on ", 99.9);
+printf("to the next chapter!\n");
+return 0;
+}
