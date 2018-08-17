ac702df65c9ac025bf7eb8bae348802446bf2fea
ianwuk <37680034+ianwuk@users.noreply.github.com>
2018-08-17 19:56:59 +0700

    Chapter 4 Start of Exercises

---
+++ Ian/Chapter 4/Chapter4ex1.c
@@ -0,0 +1,17 @@
+//Absolute Beginner's Guide to C, 3rd Edition
+//Chapter 4 Example 1 -- Chapter4ex1.c
+
+#include <stdio.h>
+
+main()
+{
+
+/* These three lines show you how to use the most popular Escape sequences */
+printf("Column A\tColumn B\tColumn C"); //tabbed columns
+printf("\nMy Computer\'s Beep Sounds Like This: \a!\n"); //will appear on a new line
+printf("Letz\bs fix that typo and then show the backslash "); //replaces z with s
+printf("character \\\" she said\n"); //appears on a new line
+
+return 0;
+
+}
