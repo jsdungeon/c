8e53e7f43bfc8ce0b92316c745b4c225b9296544
ianwuk <37680034+ianwuk@users.noreply.github.com>
2018-08-20 20:03:27 +0700

    Exercises and header files for Chapter 7
    All about using CONSTANTS and making header files that can be included in the main C file.

---
+++ Ian/Chapter 7/Chapter7ex1.c
@@ -0,0 +1,33 @@
+//Example program #1 from Chapter 7 of Absolute Beginner's Guide to
+//C 3rd Edition
+//File Chapter7ex1.c
+
+/*This is a sample program that lists the three kids and their school
+supply needs, as well as the costs to buy the supplies */
+
+#include <stdio.h>
+#include <string.h>
+#include "Chapter7ex1.h"
+
+main()
+{
+
+int age;
+char childname[14] = "Thomas";
+
+printf("\n%s have %d kids.\n", FAMILY, KIDS);
+
+age = 11;
+printf("The oldest, %s, is %d.\n", childname, age);
+
+strcpy(childname, "Christopher");
+age = 6;
+printf("The middle boy, %s, is %d.\n", childname, age);
+
+age = 3;
+strcpy(childname, "Benjamin");
+printf("The youngest, %s, is %d.\n", childname, age);
+
+return 0;
+
+}
